205a97f58746c8faa4f1137ba70de47b9e5d2e70
99rockets <makhetov@gmail.com>
2017-07-12 23:41:18 +0300

    experiment with RabbitMQ

--- server/server.js
+++ server/server.js
@@ -1,28 +1,59 @@
+const http = require('http')
 const express = require('express')
 const amqp = require('amqplib/callback_api')
 
-const app = express()
+amqp.connect('amqp://rabbitmq:rabbitmq@rabbit_1:5672', (err, conn) => {
+    if (err) console.error(err)
 
-const rabbitConnection = amqp.connect('amqp://rabbitmq:rabbitmq@rabbit_1:5672', (err, conn) => {
-    if (err) {
-        console.error(err)
-    } else {
-        console.info('Connected to RabbitMQ')
-    }
-})
+    const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    conn.createChannel((err, channel) => {
+        if (err) console.error(err)
+        const Queue = "test"
 
-app.get('/send', (req, res) => {
-    res.sendStatus(200)
-})
+        channel.assertQueue(Queue, {durable: true})
 
-app.get('/list', (req, res) => {
-    res.sendStatus(200)
-})
+        app.get('/', (req, res) => {
+            res.send('Welcome to server')
+        })
 
-app.listen(8080, () => {
-    console.log('Demo app listening on port 8080!')
+        app.get('/send', (req, res) => {
+            channel.sendToQueue(Queue, new Buffer(JSON.stringify("testme")))
+            console.info(`Published message on channel ${Queue}`)
+            res.sendStatus(200)
+        })
+
+        app.get('/:number', (req, res) => {
+            const number = req.params.number
+            const start = new Date()
+            for (var i=0; i < number; i++) {
+                channel.sendToQueue(Queue, new Buffer(JSON.stringify(`Task #${i}`)))
+            }
+            const end = new Date() - start
+            res.send(`${number} tasks recived to RabbitMQ; Performance time: ${(end/1000)%60} sec`)
+        })
+
+        channel.consume(Queue, (msg) => {
+            const text = msg.content.toString('utf8')
+
+            return new Promise((resolve, reject) => {
+                const options = {
+                    hostname: '192.168.99.100',
+                    port: 8080
+                }
+                http.get(options, (res) => {
+                    console.log(`Recived: ${text}`)
+                    resolve(res.statusCode)
+                })
+            })
+
+            // setTimeout(() => {
+            //     console.log(`Recived: ${text}`)
+            // }, 4000)
+        }, {noAck: true})
+
+        app.listen(8080, () => {
+            console.log('Demo app listening on port 8080!')
+        })
+    })
 })
